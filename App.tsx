29add7f0a2140de20e57cf1e9427647378fa40a2
unknown <orealaslam@gmail.com>
2023-11-29 12:54:23 +0500

    check internet status

--- App.tsx
+++ App.tsx
@@ -4,10 +4,26 @@ import {NavigationContainer} from '@react-navigation/native';
 import Route from './src/routes/Route';
 import {get_async_data} from './src/Helper/AppHelper';
 import MainRoute from './src/routes/MainRoute';
+import {useNetInfo} from '@react-native-community/netinfo';
+import {
+  View,
+  BackHandler,
+  Text,
+  TouchableOpacity,
+  Image,
+  Dimensions,
+} from 'react-native';
+import {NetworkModelStyle} from './src/Helper/StyleHelper';
+
+const {width, height} = Dimensions.get('window');
+const buttonWidth = width - 200;
+const buttonratio = buttonWidth / 480;
 
 const App = (navigation: any) => {
+  const {type, isConnected} = useNetInfo();
   const [splashClosed, setsplashClosed] = useState(false);
   const [userid, setuserid] = useState(null);
+  const [NetworkModel, setNetworkModel] = useState(false);
 
   useEffect(() => {
     (async () => {
@@ -18,10 +34,42 @@ const App = (navigation: any) => {
     })();
   }, []);
 
+  useEffect(() => {
+    if (isConnected) {
+      setNetworkModel(false);
+    } else {
+      setNetworkModel(true);
+    }
+  }, [isConnected]);
+
   return (
     <NavigationContainer>
       {splashClosed ? (
-        <>{userid != null ? <MainRoute></MainRoute> : <Route></Route>}</>
+        <>
+          {NetworkModel == true ? (
+            <View style={NetworkModelStyle.container}>
+              <View style={NetworkModelStyle.modelContainer}>
+                <Text style={NetworkModelStyle.heading}>Network Error</Text>
+                <Text style={NetworkModelStyle.description}>
+                  Failed to connect to Internet. Try again Later!
+                </Text>
+                <TouchableOpacity onPress={()=>BackHandler.exitApp()}>
+                  <Image
+                    style={{
+                      alignSelf: 'center',
+                      width: buttonWidth,
+                      height: 148 * buttonratio,
+                      marginTop: 20,
+                    }}
+                    source={require('./src/assets/exitApp.png')}
+                  />
+                </TouchableOpacity>
+              </View>
+            </View>
+          ) : (
+            <>{userid != null ? <MainRoute></MainRoute> : <Route></Route>}</>
+          )}
+        </>
       ) : null}
     </NavigationContainer>
   );
